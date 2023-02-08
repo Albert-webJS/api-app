fc95598ab5cf05dd2c5ae0e4c66ca12e10d24b38
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-02-08 17:47:55 +0200

    add error class for app

---
+++ src/errors/exeption.filter.interface.ts
@@ -0,0 +1,5 @@
+import { NextFunction, Response, Request } from "express";
+
+export interface IExeptionFilter {
+    catch: (error: Error, request: Request, response: Response, next: NextFunction) => void
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { App } from "./app";
+import { ExeptionFilter } from "./errors/exeption.filter";
 import { LoggerService } from "./logger";
 import { UserController } from "./users";
 
 const bootstrapApplication = async (): Promise<void> => {
   const logger = new LoggerService()
-  const app = new App(logger, new UserController(logger));
+  const app = new App(
+    logger,
+    new UserController(logger),
+    new ExeptionFilter(logger)
+  );
   await app.init();
 };
 
